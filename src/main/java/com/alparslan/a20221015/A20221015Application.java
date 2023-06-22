40ec16d4164896ba75253c301f214d9fa4e46a76
AlparslanKose <alparslan.kosee@gmail.com>
2023-06-22 20:11:36 +0300

    Spring1.01

---
+++ src/main/java/com/alparslan/a20221015/A20221015Application.java
@@ -0,0 +1,13 @@
+package com.alparslan.a20221015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A20221015Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A20221015Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/alparslan/a20221015/Greeting.java
@@ -0,0 +1,3 @@
+package com.alparslan.a20221015;
+
+public record Greeting(long id, String content) { }
